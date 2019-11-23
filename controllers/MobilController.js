576c489e719e785b9831398e97d4a32b6e8c7b7e
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-11-24 04:21:19 +0700

    get data admin lte

--- controllers/MobilController.js
+++ controllers/MobilController.js
@@ -1,5 +1,9 @@
 const {
-  Car
+  Car,
+  Bank,
+  Jadwal,
+  Customer,
+  Pesanan
 } = require("../models")
 const Op = require("sequelize").Op
 
@@ -14,22 +18,38 @@ exports.getMobil = async (req, res) => {
 
 // disini cara ambil view yat ???
 exports.getJadwal = async (req, res) => {
+  const jadwal = await Jadwal.findAll({
+    include: [
+      { model: Car }
+    ],
+  })
   res.render('admin/jadwal/view_jadwal', {
-    title: "Mobil",
+    title: "Jadwal",
+    jadwal
   })
 };
 
 // disini cara ambil view yat ???
 exports.getCustomer = async (req, res) => {
+  const user = await Customer.findAll()
+  console.log(user)
   res.render('admin/customer/view_customer', {
     title: "Customer",
+    user
   })
 };
 
 // disini cara ambil view yat ???
 exports.getPesanan = async (req, res) => {
+  const pesanan = await Pesanan.findAll({
+    include: [
+      { model: Jadwal },
+      { model: Customer }
+    ],
+  })
   res.render('admin/pesanan/view_pesanan', {
     title: "Pesanan",
+    pesanan
   })
 };
 
@@ -42,8 +62,10 @@ exports.getPembayaran = async (req, res) => {
 
 // disini cara ambil view yat ???
 exports.getBank = async (req, res) => {
+  const bank = await Bank.findAll()
   res.render('admin/bank/view_bank', {
     title: "Bank",
+    bank
   })
 };
 
