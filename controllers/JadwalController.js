acc668a7575b589bd2dd73d1dbf9dd559597d3bf
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-11-24 14:49:21 +0700

    get find jadwal

--- controllers/JadwalController.js
+++ controllers/JadwalController.js
@@ -5,25 +5,27 @@ const {
 const Op = require("sequelize").Op
 
 exports.getJadwal = (req, res) => {
-  const {
-    tanggal_berangkat,
-    origin,
-    destination,
-    jam_berangkat
-  } = req.body
-
   Jadwal.findAll({
-    // where: {
-    //   tanggal_berangkat: { [Op.eq]: tanggal_berangkat },
-    //   origin: { [Op.eq]: origin },
-    //   destination: { [Op.eq]: destination },
-    //   jam_berangkat: { [Op.eq]: jam_berangkat }
-    // },
     include: [
       { model: Car }
     ],
   }).then((jadwal) => {
     res.status(200).json({ data: jadwal, message: "Success Read Jadwal" });
+  }).catch((err) => {
+    res.status(500).json({ message: "Internal server error" });
+  });
+};
+
+
+exports.getTanggalJadwal = (req, res) => {
+  const { tanggal_berangkat } = req.params
+  Jadwal.findAll({
+    where: { tanggal_berangkat: tanggal_berangkat },
+    include: [
+      { model: Car }
+    ],
+  }).then((jadwal) => {
+    res.status(200).json({ data: jadwal, message: "Success Read Jadwal Pertanggal" });
   }).catch((err) => {
     res.status(500).json({ message: "Internal server error" });
   });
--- routes/jadwal.js
+++ routes/jadwal.js
@@ -2,9 +2,11 @@ var express = require("express");
 var router = express.Router();
 var {
   getJadwal,
+  getTanggalJadwal
 } = require("../controllers/JadwalController");
 
 /* get all address */
 router.get("/", getJadwal);
+router.get("/:tanggal_berangkat", getTanggalJadwal);
 
 module.exports = router;
